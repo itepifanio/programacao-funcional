840f12c09a0d0f236932515060760bd97c71593f
italoepifaniols@gmaiL.com <italoepifaniols@gmail.com>
2018-05-28 17:16:46 -0300

    Modifica aula 24

--- aulas/A21.hs
+++ aulas/A21.hs
@@ -1,3 +1,4 @@
+{-
 module Trees where
 
 data Tree a = Leaf a | Node (Tree a) a (Tree a)
@@ -10,3 +11,22 @@ flatten (Node l x r) = flatten l ++ [x] ++ flatten r
 tmap :: (a -> b) -> Tree a -> Tree b
 tmap f (Leaf x)     = Leaf (f x)
 tmap f (Node l x r) = Node (tmap f l) (f x) (tmap f r)
+-}
+
+module Maybe where
+
+import Prelude hiding (Maybe(..), Either)
+data Maybe a = Nothing | Just a
+    deriving (Eq, Show)
+
+data Either a b = Left a | Right b
+ 
+
+bottom :: a
+bottom = bottom
+
+safeHead :: [a] -> Maybe a
+safeHead []     = Nothing 
+safeHead (x:_)  = Just x
+
+
