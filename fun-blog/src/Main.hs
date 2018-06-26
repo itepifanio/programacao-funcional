cb3fca407bdc1d66c83cb57488734e6797ce088a
Ítalo Epifânio <italoepifaniols@gmail.com>
2018-06-26 15:02:52 -0300

    Renderizando paginas web

--- fun-blog/src/Main.hs
+++ fun-blog/src/Main.hs
@@ -1,6 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 import Web.Scotty
+import qualified Data.Text.Lazy as T
+import qualified Text.Blaze.Html5 as H
+
+data Post = Post 
+    { tipo :: T.Text
+    , titulo :: T.Text
+    , conteudo :: H.Html 
+    }
+
+instance Show Post where
+    show post = 
+        T.unpack $ titulo post
+
+toPost :: T.Text -> Maybe Post
+
 
 main :: IO ()
 main = do
@@ -8,3 +23,5 @@ main = do
     scotty 3000 $ do
         get "/" $ do
             text "hello world!"
+        get "/home" $ file "templates/index.html"
+            
